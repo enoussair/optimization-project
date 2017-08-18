9764ae80847fca6e1b2bf3ee80b482d85864f056
Noussair Erassifi <n.erassifi@gmail.com>
2017-08-18 14:26:49 -0500

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 
 gulp.task('imagemin', function(){
-  return gulp.src('img/*')
-          .pipe(imagemin())
+  return gulp.src('**/*.{jpg,png}')
+          .pipe(imagemin([
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5})
+          ]))
           .pipe(gulp.dest('dist/img'));
 });
